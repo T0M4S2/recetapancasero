164535d777d87ced1daf52a7206c2f39318b2a42
tomas <tomiescobar.19@gmail.com>
2021-08-21 21:32:16 -0300

    subiendo pagina de pan casero

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let bntMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+bntMenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar')
+});
